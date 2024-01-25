cf7c1350049e6922c7002b3550faf50f6df644aa
Ashokraj Dhole <88529223+ashokrajdhole@users.noreply.github.com>
2024-01-25 20:45:42 +0530

    Add files via upload

---
+++ Python/data_types.py
@@ -0,0 +1,6 @@
+#We will learn Data Types in python 
+#   1)Numbers
+#   2)String
+#   3)Tuple
+#   4)List
+#   5)Dictionary
---
+++ Python/start.py
@@ -0,0 +1,17 @@
+#First program for python programming
+
+print("Welcome to python")  #This is the syntax for priting anything
+
+#Using basic operation in python programming
+a=10
+b=20
+print("Addition of a and b is ->",(a+b))
+
+#If we want to pass value in python we will do this thing
+print("Enter your value here:")
+enter=input() #used to accept input from user
+print("Your value is",enter)
+
+#Another way to give inputs
+value =int(input("Enter you value here: ")) #Used to take inputs 
+print(value) #Printing The value give by user
